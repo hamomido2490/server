dcafa30c9934afdb66b18ce903aa82748ed4b05b
Secrets Room <hamomido2490@gmail.com>
2025-08-12 21:20:37 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
 // تحميل متغيرات البيئة من ملف .env (في التطوير فقط)
 dotenv.config();
